2c2f0bc155c5221ecd3bdd503f3459f0b013e461
GiovannaSuarez <141734522+GiovannaSuarez@users.noreply.github.com>
2023-08-16 22:25:02 -0300

    Add files via upload

---
+++ ejercicios_complementarios.py
@@ -0,0 +1,71 @@
+#EJERCICIO 1
+numero1=int(10)
+
+#EJERCICIO 2
+numero2=float(1.2)
+
+#EJERCICIO 3
+suma=(numero1+numero2)
+
+#EJERCICIO 4
+print("El número 1 es: ",numero1)
+print("El número 2 es: ",numero2)
+resta=(numero1-numero2)
+multiplicacion=(numero1*numero2)
+division=(numero1/numero2)
+print("La resta de los dos números es: ",resta)
+print("La división de los dos números es: ",division)
+print("La multiplicacion de los dos números es: ",multiplicacion)
+
+#EJERCICIO 5
+nombre="Giovanna"
+
+#EJERCICIO 6
+precio=1011.99
+print("El precio del producto es: ",precio)
+#EJERCICIO 7
+descuento=0.15
+
+#EJERCICIO 8
+precio_final= precio-(precio*descuento)
+print("El precio del producto con el descuento del ",descuento,"% es: ",precio_final)
+
+#EJERCICIO 9
+cadena=("Hola profe")
+
+#EJERCICIO 10
+longitud=len(cadena)
+print("La frase <<",cadena,">> tiene ",longitud," caracteres")
+
+#EJERCICIO 11
+precio=int(10.99)
+print("El precio aproximado es: ",precio)
+
+#EJERCICIO 12
+nombre="Giovanna"
+apellido="Suarez"
+nombre_completo=nombre+" "+apellido
+print("El nombre completo es: ",nombre_completo)
+
+#EJERCICIO 13
+mi_edad=23
+mi_edad+=5
+print("Mi edad es 23 años pero, incrementado en 5 es: ",mi_edad)
+mi_edad=23
+mi_edad-=10
+print("Y disminuida en 10 es: ",mi_edad)
+
+#EJERCICIO 14
+altura=1.55
+print("Mi altura es: ",altura)
+altura=(altura*4)/3
+print("(1.55 x 4)/3 = ",altura)
+
+#EJERCICIO 15
+nombre_completo="GIOVANNA SUAREZ"
+print("Mi nombre en mayúsculas: ",nombre_completo)
+print("Mi nombre en minúsculas: ",nombre_completo.lower())
+
+#EJERCICIO 16
+print("Mayúsculas y minúsculas: "+nombre_completo[0]+nombre_completo[1:15].lower())
+
